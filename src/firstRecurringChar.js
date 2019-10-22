e5d25b7f32a7904591b6757f6bcb062dafd07d15
Sonia Dumitru <soni.dumitru@gmail.com>
2019-10-22 12:54:06 -0500

    firstRecurringChar

---
+++ src/firstRecurringChar.js
@@ -0,0 +1,3 @@
+// Write a function that when given a string, will return the first recurring character in it, or null if there is no recurring character.
+// For example, given the string "acbbac", return "b". Given the string "abcdef", return null.
+// You can assume all inputs will be non-empty strings of english characters.
