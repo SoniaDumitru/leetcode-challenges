7213e75b7ca12aa5aa48ffd568482d13c281df85
Sonia Dumitru <soni.dumitru@gmail.com>
2019-10-26 13:31:55 -0500

    implementStr

--- src/implementStr.js
+++ src/implementStr.js
@@ -2,13 +2,11 @@
 
 let haystack = "hello";
 let needle = "ll";
-var strStr = function(haystack, needle) {
+let strStr = function(haystack, needle) {
   if (!needle) return 0
-
   for (let i = 0 ; i < haystack.length ; i++) {
     if (haystack.slice(i, i+needle.length) === needle) return i
   }
-
   return -1
 }
 
