f7b6eec70692c64761c6f4bc52c209d9a4314168
Sonia Dumitru <soni.dumitru@gmail.com>
2019-12-13 14:54:45 -0600

    backspace string compare

--- src/flippingAnImage.js
+++ src/flippingAnImage.js
@@ -1,10 +1,10 @@
-// Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+// Given a binary matrix array, we want to flip the image horizontally, then invert it, and return the resulting image.
 // To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
 // To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
 
-var flipAndInvertImage = function(A) {
-    let reversed = A.map((currArray, index) => {
-       return currArray.reverse();
+function flipAndInvertImage(array) {
+    let reversed = array.map((currentArray, index) => {
+       return currentArray.reverse();
     })
 
     for (let i=0; i<reversed.length; i++) {
@@ -17,5 +17,5 @@ var flipAndInvertImage = function(A) {
         }
     }
 
-    return reversed
+    return reversed;
 };
