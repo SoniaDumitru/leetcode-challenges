77550aa03d26a3a55422b30025251fe1ec00aced
Sonia Dumitru <soni.dumitru@gmail.com>
2020-01-15 10:21:18 -0600

    second commit

--- src/reverseOnlyLetters.js
+++ src/reverseOnlyLetters.js
@@ -2,14 +2,16 @@
 
 let string = 's-o09-a'
 function reverseOnlyLetters(string) {
-  let reverse= string.replace(/[^a-zA-Z]/g, "").split('').reverse().join('').split('');
+  let reverse = string.replace(/[^a-zA-Z]/g, "").split('').reverse();
+  // console.log(reverse)
 
   for(let i=0; i<string.length; i++) {
-       if(!(string[i].toLowerCase() <= 'z' && string[i].toLowerCase() >= "a")) {
+       if(!(string[i].toLowerCase() <= "z" && string[i].toLowerCase() >= "a")) {
         reverse.splice(i, 0, string[i]);
+        console.log(reverse)
       }
     }
   return reverse.join('')
 };
 
-// reverseOnlyLetters(string) => a-o09-s
+// reverseOnlyLetters(string)
