953ee364340f4c25509701e17281efa8a781ccc4
Sonia Dumitru <soni.dumitru@gmail.com>
2020-01-23 20:03:42 -0600

    minTimeVisitedAllPoints second commit

--- src/minTimeVisitingAllPoints.js
+++ src/minTimeVisitingAllPoints.js
@@ -2,3 +2,17 @@
 // You can move according to the next rules:
 // In one second always you can either move vertically, horizontally by one unit or diagonally (it means to move one unit vertically and one unit horizontally in one second).
 // You have to visit the points in the same order as they appear in the array.
+
+let points = [[1,1],[3,4],[-1,0]];
+
+function minTimeToVisitAllPoints(points) {
+  let times = 0;
+  for (let i = 1; i < points.length; i++) {
+    const p_zero = points[i - 1];
+    const p_one = points[i];
+    times += Math.max(Math.abs(p_one[0] - p_zero[0]), Math.abs(p1[1] - p0[1]));
+  }
+  return times;
+};
+
+minTimeToVisitAllPoints(points);
