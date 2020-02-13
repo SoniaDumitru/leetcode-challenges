b2b7fc3510f881ffab74b21d7f043ad079d600ae
Sonia Dumitru <soni.dumitru@gmail.com>
2020-02-12 23:41:21 -0600

    recursive solution

--- src/graphPathChecker.js
+++ src/graphPathChecker.js
@@ -15,3 +15,43 @@
       //   d <-- c
 
 // The doesPathExist function we'll be writing will be passed an adjacency map as well as two strings indicating the starting node and ending node. The function will return a boolean indicating if a path exists from the starting to the ending node:
+
+// First example:
+// function doesPathExist(graph, start, end) {
+//   // your code here
+// }
+//
+// doesPathExist(graph, 'a', 'b') // true
+// doesPathExist(graph, 'b', 'a') // false
+// doesPathExist(graph, 'a', 'd') // true
+// doesPathExist(graph, 'a', 'a') // false
+
+// Second Example:
+// Here are some examples for another graph. Notice that this graph contains a node that points to itself and also a 'cycle' (that is, a loop involving multiple nodes):
+// const graph = {
+//   a: ['a', 'c'],
+//   c: ['r', 's'],
+//   r: ['a'],
+//   s: []
+// }
+//
+// doesPathExist(graph, 'a', 'a') // true
+// doesPathExist(graph, 'c', 'c') // true
+// doesPathExist(graph, 'r', 's') // true
+// doesPathExist(graph, 's', 'a') // false
+
+const doesPathExist = (graph, start, target, visited = {}) => {
+  if (!graph[start]) return false
+  visited[start] = true;
+
+  return graph[start].some((vertex) => {
+    if (vertex === target) return true;
+    if (!visited[vertex]) {
+      return doesPathExist(graph, vertex, targeet, visited)
+    } else {
+      return false;
+    }
+  })
+}
+
+// Note that we're using an object to keep track of visted notes. Note also the way in which we initialize an additional argument to allow us to pass this object to the recursive function invocation-- this is a useful trick that lets us not pollute global scope while avoiding the need for a helper function.
