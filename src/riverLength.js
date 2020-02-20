a80eed39e0fdb4361d53aa178326193993610bc7
Sonia Dumitru <soni.dumitru@gmail.com>
2020-02-20 00:15:17 -0600

    river length completed

--- src/riverLength.js
+++ src/riverLength.js
@@ -2,7 +2,7 @@
 // This array represents a map: 0s are land, and 1s are water.
 // A "river" on this map consists of any number of contiguous,
 // adjacent water squares, where "adjacent" means "above", "below", "to the left of", or "to the right of" (that is, diagonal squares are not adjacent).
-// Write a function which returns an array of the sizes of all rivers represented in the input matrix.
+// Write a function which returns an array of the sizes of all rivers represented in the arr matrix.
 
 Example:
 
@@ -30,3 +30,18 @@ const riverLengths = (arr) => {
 }
 
 // Inner function, checkNeighbors recursively follows the length of a river, determining its length and returning this number to its caller, whether riverLengths or checkNeighbors
+
+const checkNeighbors = (x, y, arr) => {
+  arr[y][x] = 0; // mark cell as visited
+  let size = 1;
+
+  [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]].forEach(([i, j]) => {
+    // make sure we don't access a row that doesn't exist &&
+    // then check to see if we have a river because 1 is truthy
+    if (arr[j] && arr[j][i]) {
+      size += checkNeighbors(i, j, arr);
+    }
+  });
+
+  return size;
+};
