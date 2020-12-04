2bee22df36c7f407a910b6b697f1472864c12b72
SoniaDumitru <sonya_dumitru@yahoo.com>
2020-12-04 22:26:16 +0200

    twoNumberSum

---
+++ src/twoNumberSum.js
@@ -0,0 +1,47 @@
+// Ex: givenArray = [1,2,3] && targetSum 5 => [2,3]
+
+// Solution 1:
+function twoNumberSum(array, targetSum) {
+    for (let i = 0; i < array.length - 1; i++) {
+          let firstNum = array[i];
+          for (let j = 0; j < array.length; j++) {
+              const secondNum = array[j];
+              if (firstNum + secondNum === targetSum) {
+                  return [firstNum, secondNum];
+              }
+          }
+      }
+      return [];
+  }
+
+// Solution 2:
+function twoNumberSum(array, targetSum) {
+	const nums = {}
+	for (const num of array) {
+		const potentialMatch = targetSum - num;
+		if (potentialMatch in nums) {
+			return [potentialMatch, num]
+		} else {
+			nums[num] = true;
+		}
+	}
+	return [];
+}
+
+// Solution 3:
+function twoNumberSum(array, targetSum) {
+    array.sort((a, b) => a-b);
+      let left = 0;
+      let right = array.length - 1;
+      while (left < right) {
+          let sum = array[left] + array[right]
+          if (sum === targetSum) {
+              return [array[left], array[right]]
+          } else if (sum < targetSum) {
+              left++;
+          } else if (sum > targetSum){
+              right--;
+          }
+      }
+      return []
+  }
